1e0f2858d4bbafcdf20119ad343d94d4cf037ee4
mockingod <mockingod@gmail.com>
2017-09-21 23:26:24 -0700

    Updated files

--- T--Washington--globalJavaScript.js
+++ T--Washington--globalJavaScript.js
@@ -6,12 +6,27 @@
     'use strict';
     
     window.onload = function() {
+        // local global variable
+        var jsonData = "";
+
         // Set up hover dropdown functionality
         $('body').on('mouseenter mouseleave','.dropdown',function(e){
             var _d=$(e.target).closest('.dropdown');_d.addClass('show');
             setTimeout(function(){
                 _d[_d.is(':hover')?'addClass':'removeClass']('show');
         },300);
+
+        // Grab JSON data
+        $.get("T--Washington--allData.json", function(data, status) {
+            if(status != "success") {
+                alert("Failed to load data");
+            } else {
+                jsonData = JSON.parse(data);
+            }
+        });
+
+        $("body").text = "testtestest"
+
     });
 
 
@@ -19,7 +34,9 @@
         
     }
 
-
+    function getJSONData() {
+        
+    }
 
 
 
