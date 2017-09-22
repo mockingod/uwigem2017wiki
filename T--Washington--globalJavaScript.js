25189fbb29506c20ecebc75d2bacad11b54893dc
mockingod <mockingod@gmail.com>
2017-09-22 00:36:08 -0700

    Updated code
    Migrating to javascript/JSON data and less hardcoded HTML

--- T--Washington--globalJavaScript.js
+++ T--Washington--globalJavaScript.js
@@ -4,41 +4,55 @@
 
 (function() {
     'use strict';
-    
+    var $$$ = function(id) { return document.getElementById(id); };
     window.onload = function() {
-        // local global variable
-        var jsonData = "";
-
         // Set up hover dropdown functionality
         $('body').on('mouseenter mouseleave','.dropdown',function(e){
             var _d=$(e.target).closest('.dropdown');_d.addClass('show');
             setTimeout(function(){
                 _d[_d.is(':hover')?'addClass':'removeClass']('show');
-        },300);
+            },300);
+        });
 
         // Grab JSON data
-        $.get("https://raw.githubusercontent.com/mockingod/uwigem2017wiki/master/T--Washington--allData.txt", function(data, status) {
+        $.get("https://raw.githubusercontent.com/mockingod/uwigem2017wiki/master/T--Washington--allData.json", function(data, status) {
             if(status != "success") {
                 alert("Failed to load data");
             } else {
-                jsonData = data;
+                loadData(JSON.parse(data));
             }
         });
 
-        alert(jsonData);
+    };
 
-    });
+    function loadData(data) {
+        loadNavbar(data.allcode.navbar)
+    }
+
+    function loadNavbar(data) {
 
 
-    function loadNavbar() {
         
+        $('<div></div>')
+            .addClass("test")
+            .append($('<div></div>')
+                .addClass("test2")
+                .append("test")
+
+        ).appendTo("#test");
+
+
+
+        //$("<div id='test'> test </div>").appendTo("#test");
+
+
+
+
+        alert(data[0].parent);
     }
 
     function getJSONData() {
         
     }
 
-
-
-    };
 })();
