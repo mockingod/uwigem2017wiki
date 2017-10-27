459520ca4392b0a77adab229ae739bc3c49321b9
mockingod <mockingod@gmail.com>
2017-10-27 15:54:47 -0700

    Bleh

--- DataCommaSplitterProgram/Thing/src/Thing.java
+++ DataCommaSplitterProgram/Thing/src/Thing.java
@@ -7,10 +7,16 @@ public class Thing {
             System.out.print("Input:");
             
             String text = input.nextLine();
+            
             String[] splitText = text.split("\t");
             
             
             String finalString = "";
+            
+            
+            
+            
+            
             for(int i = 0; i < splitText.length; i++) {
                 finalString = finalString + splitText[i] + ",";
             }
