881b0b036c0383e14c8d53f197ad86ac3eab5ea0
mockingod <mockingod@gmail.com>
2017-10-27 15:48:56 -0700

    Added comma splitter for copy pasting data from excel to chart javascript json

---
+++ DataCommaSplitterProgram/Thing/src/Thing.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Thing {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while(true) {
+            System.out.print("Input:");
+            
+            String text = input.nextLine();
+            String[] splitText = text.split("\t");
+            
+            
+            String finalString = "";
+            for(int i = 0; i < splitText.length; i++) {
+                finalString = finalString + splitText[i] + ",";
+            }
+            finalString = finalString.substring(0,finalString.length()-1);
+            
+            System.out.println(finalString);
+            
+        }
+    }
+}
